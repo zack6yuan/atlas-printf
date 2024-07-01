e3a152fe1ef340a881df2843fcc65147363630af
zack6yuan <zack6yuan@gmail.com>
2024-07-01 19:16:47 +0000

    structure

--- lens.c
+++ lens.c
@@ -5,14 +5,14 @@
 int main()
 {
     char Str[1000];
-    int i;
+    int i; /*iterator*/
 
     printf("Enter the String: ");
-    scanf("%s", Str);
+    scanf("%s", Str); /*stores user input*/
 
-    for (i = 0; Str[i] != '\0'; ++i);
+    for (i = 0; Str[i] != '\0'; ++i); /*iterates through string*/
 
-    printf("Length of Str is %d", i);
+    printf("Length of Str is %d", i); /*prints length of string*/
 
     return 0;
 }
